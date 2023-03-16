936e20609de79214dfe60caca51384d562ec59a4
speed1313 <speedtry13@icloud.com>
2023-03-16 16:23:31 +0900

    refactoring

--- pkgsAnalysis/main.go
+++ pkgsAnalysis/main.go
@@ -24,6 +24,8 @@ func main() {
 	// read one line from response.Body
 	scanner := bufio.NewScanner(response.Body)
 	impoertedByCount := 0
+	// int64 enumCounter
+	var enumCount int64 = 0
 	for scanner.Scan() {
 		// read one line from response.Body
 		line := scanner.Text()
@@ -36,71 +38,13 @@ func main() {
 			url, _ = strings.CutPrefix(url, "/")
 
 			fmt.Println(url)
-			// hash of url
-			dir := sha256.Sum256([]byte(url))
-			// dir change to string
-			dir_name := fmt.Sprintf("%x", dir[:8])
-			// create dir
-			err := os.MkdirAll(dir_name, 0755)
-			if err != nil {
-				log.Fatal("mkdir failed: ", err)
-			}
 
-			// cd dir
-			err = os.Chdir(dir_name)
-			if err != nil {
-				log.Fatal("cd failed: ", err)
-			}
-			// create go.mod
-			if _, err := os.Stat("go.mod"); os.IsNotExist(err) {
-
-				err = exec.Command("go", "mod", "init", "a").Run()
-				if err != nil {
-					log.Fatal("go mod init failed: ", err)
-				}
-			}
-			// make file and write url
-			f, err := os.Create("tmp.go")
-			if err != nil {
-				log.Fatal("create tmp.go failed: ", err)
-			}
-			_, err = f.WriteString("package a\nimport \"" + url + "\"")
-			if err != nil {
-				log.Fatal("write tmp.go failed: ", err)
-			}
-
-			err = exec.Command("go", "mod", "tidy").Run()
-
-			if err != nil {
-				log.Fatal("go mod tidy failed: ", err)
-			}
+			prepareWorkSpace(url)
 			fmt.Println("downloaded package")
 			// "go vet  $(go list -f '{{.Dir}}' $(go list -f '{{join .Deps "\n"}}' a))"
-			cmd := exec.Command("zsh", "-c", `go list -f '{{.Dir}}' $(go list -f '{{join .Deps "\n"}}' a)`)
-			pkglist, err := cmd.Output()
-			if err != nil {
-				log.Fatal("go list failed: ", err)
-			}
-			for _, dir := range strings.Split(string(pkglist), "\n") {
-				if err != nil {
-					log.Fatal("go list failed: ", err)
-				}
-				cmd = exec.Command("zsh", "-c", `go vet -vettool=$(which enumResearch) ` +  dir)
-				out, err := cmd.CombinedOutput()
-				if err != nil {
-					print("vet output:", string(out))
-				}
-			}
-
-			err = os.Chdir("..")
-			if err != nil {
-				log.Fatal("cd failed: ", err)
-			}
-			// remove dir
-			err = os.RemoveAll(dir_name)
-			if err != nil {
-				log.Fatal("remove failed: ", err)
-			}
+			hashDir := sha256.Sum256([]byte(url))
+			dir := fmt.Sprintf("%x", hashDir[:8])
+			doVet(dir, &enumCount)
 
 			if impoertedByCount > 5 {
 				break
@@ -110,6 +54,73 @@ func main() {
 
 	}
 	fmt.Printf("ImportedBy count: %d\n", impoertedByCount)
-	enum := 0
-	print(enum)
+	fmt.Printf("enum count: %d\n", enumCount)
+}
+
+
+func prepareWorkSpace(pkgName string) {
+	hashDir := sha256.Sum256([]byte(pkgName))
+	// dir change to string
+	dir := fmt.Sprintf("%x", hashDir[:8])
+	// create dir
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		log.Fatal("mkdir failed: ", err)
+	}
+	// cd dir
+	err = os.Chdir(dir)
+	if err != nil {
+		log.Fatal("cd failed: ", err)
+	}
+	// create go.mod
+	if _, err := os.Stat("go.mod"); os.IsNotExist(err) {
+		err = exec.Command("go", "mod", "init", "a").Run()
+		if err != nil {
+			log.Fatal("go mod init failed: ", err)
+		}
+	}
+	// make file and write url
+	f, err := os.Create("tmp.go")
+	if err != nil {
+		log.Fatal("create tmp.go failed: ", err)
+	}
+	_, err = f.WriteString("package a\nimport \"" + pkgName + "\"")
+	if err != nil {
+		log.Fatal("write tmp.go failed: ", err)
+	}
+	err = exec.Command("go", "mod", "tidy").Run()
+
+	if err != nil {
+		log.Fatal("go mod tidy failed: ", err)
+	}
+}
+
+func doVet(dir string, enumCount *int64) {
+	// "go vet  $(go list -f '{{.Dir}}' $(go list -f '{{join .Deps "\n"}}' a))"
+	cmd := exec.Command("zsh", "-c", `go list -f '{{.Dir}}' $(go list -f '{{join .Deps "\n"}}' a)`)
+	pkglist, err := cmd.Output()
+	if err != nil {
+		log.Fatal("go list failed: ", err)
+	}
+	for _, dir := range strings.Split(string(pkglist), "\n") {
+		if err != nil {
+			log.Fatal("go list failed: ", err)
+		}
+		cmd = exec.Command("zsh", "-c", `go vet -vettool=$(which enumResearch) ` +  dir)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			print("vet output:", string(out))
+			*enumCount++
+		}
+	}
+
+	err = os.Chdir("..")
+	if err != nil {
+		log.Fatal("cd failed: ", err)
+	}
+	// remove dir
+	err = os.RemoveAll(dir)
+	if err != nil {
+		log.Fatal("remove failed: ", err)
+	}
 }
